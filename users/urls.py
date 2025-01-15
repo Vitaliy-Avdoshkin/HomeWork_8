58cddcbadf8febf1d285f9dbb5e9a94f605139c2
AKVA <wioswa@gmail.com>
2025-01-15 17:28:21 +0300

    hw_8_31 START

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet
+from users.views import PaymentViewSet, UserCreateAPIView
 
 app_name = UsersConfig.name
 
@@ -11,5 +13,8 @@ router.register(r"payments", PaymentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('register/',UserCreateAPIView.as_view(), name= 'register'),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 urlpatterns += router.urls
