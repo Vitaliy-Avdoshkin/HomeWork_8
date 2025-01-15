462c82568ede786799a65d28be434714b22c7ea7
AKVA <wioswa@gmail.com>
2025-01-15 18:26:21 +0300

    hw_8_31 || T1 is DONE

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
@@ -13,8 +14,8 @@ router.register(r"payments", PaymentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('register/',UserCreateAPIView.as_view(), name= 'register'),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
 ]
 urlpatterns += router.urls
