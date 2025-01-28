166f650b94ef57301204251fa1cec2f09252f061
AKVA <wioswa@gmail.com>
2025-01-28 07:10:48 +0300

    hw_8_32.1 is DONE

--- lms/tests.py
+++ lms/tests.py
@@ -1,9 +1,13 @@
+from unittest import TestCase
+
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from lms.models import Course, Lesson, Subscription
 from users.models import User
+
+TestCase.maxDiff = None
 
 
 class LessonTestCase(APITestCase):
@@ -23,7 +27,6 @@ class LessonTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(data.get("name"), self.lesson.name)
 
-
     def test_lesson_create(self):
         user2 = User.objects.create(email="test2@example.com")
         course2 = Course.objects.create(name="Python", user=user2)
@@ -32,7 +35,8 @@ class LessonTestCase(APITestCase):
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Lesson.objects.count(), 2)
-#
+
+    #
     def test_lesson_update(self):
         url = reverse("lms:Lesson_update", args=(self.lesson.pk,))
         data = {
@@ -41,73 +45,66 @@ class LessonTestCase(APITestCase):
         response = self.client.patch(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(data.get("name"), "Python_update")
-#
+
+    #
     def test_lesson_delete(self):
         url = reverse("lms:Lesson_destroy", args=(self.lesson.pk,))
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Lesson.objects.count(), 0)
-#
+
+    #
     def test_lesson_list(self):
         url = reverse("lms:Lessons_list")
-        data = {
-    "count": 1,
-    "next": None,
-    "previous": None,
-    "results": [
-        {
-            "id": self.,
-            "lessons_quantity": 1,
-            "lessons_info": [
+        response = self.client.get(url)
+        data = response.json()
+        result = {
+            "count": 1,
+            "next": None,
+            "previous": None,
+            "results": [
                 {
-                    "id": 1,
-                    "name": "DJANGO_POSTMAN",
-                    "description": null,
-                    "picture": null,
-                    "video_link": null,
-                    "course": 3,
-                    "user": null
+                    "id": self.lesson.pk,
+                    "name": "Lesson_test",
+                    "description": None,
+                    "picture": None,
+                    "video_link": None,
+                    "course": self.course.pk,
+                    "user": self.user.pk,
                 }
             ],
-            "name": "SKYPRO_8",
-            "description": null,
-            "preview": null,
-            "user": null
         }
-    ]
-}
-        response = self.client.get(url)
-        print(response.json())
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-#
-#
-# class SubscriptionTastCase(APITestCase):
-#
-#     def setUp(self):
-#         self.user = User.objects.create(email="test@example.com", password="123qwe")
-#         self.course = Course.objects.create(name="Test", user=self.user)
-#         self.client.force_authenticate(user=self.user)
-#
-#     def test_subscription_create(self):
-#         url = reverse("lms:Subscription_create")
-#         data = {
-#             "course": self.course.pk,
-#         }
-#         response = self.client.post(url, data)
-#         data = response.json()
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(Subscription.objects.count(), 1)
-#         self.assertEqual(data, {"message": "Подписка создана"})
-#
-#     def test_subscription_delete(self):
-#         url = reverse("lms:Subscription_create")
-#         data = {
-#             "course": self.course.pk,
-#         }
-#         Subscription.objects.create(user=self.user, course=self.course)
-#         response = self.client.post(url, data)
-#         data = response.json()
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(Subscription.objects.count(), 0)
-#         self.assertEqual(data, {"message": "Подписка удалена"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data, result)
+
+
+class SubscriptionTastCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="test@example.com", password="123qwe")
+        self.course = Course.objects.create(name="Test", user=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_subscription_create(self):
+        url = reverse("lms:Subscription_create")
+        data = {
+            "course": self.course.pk,
+        }
+        response = self.client.post(url, data)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Subscription.objects.count(), 1)
+        self.assertEqual(data, {"message": "Подписка создана"})
+
+    def test_subscription_delete(self):
+        url = reverse("lms:Subscription_create")
+        data = {
+            "course": self.course.pk,
+        }
+        Subscription.objects.create(user=self.user, course=self.course)
+        response = self.client.post(url, data)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Subscription.objects.count(), 0)
+        self.assertEqual(data, {"message": "Подписка удалена"})
