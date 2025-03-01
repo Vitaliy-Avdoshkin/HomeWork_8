1db0bf5aec21504afc674f370ec3d0507df550a4
AKVA <wioswa@gmail.com>
2025-03-01 19:17:48 +0300

    hw_9_35.2 || add nginx

--- config/settings.py
+++ config/settings.py
@@ -169,3 +169,13 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
 EMAIL_ADMIN = EMAIL_HOST_USER
+
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test_db.sqlite3',
+        }
+
+    }
